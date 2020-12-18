c9f3f2878e82acbcca1db62e76f54bb9d3aa10cb
Danila <fdanya0@gmail.com>
2020-12-18 21:11:51 +0300

    refactor model smartphone

--- Store/Models/Products/Smartphone.cs
+++ Store/Models/Products/Smartphone.cs
@@ -5,11 +5,10 @@ using System.Threading.Tasks;
 
 namespace Store.Models.Products
 {
-    public class Smartphone
+    public class Smartphone:Electronics
     {
         public string Resolution { get; set; }
         public int Cores { get; set; }
-        public string Name { get; set; }
         public int BatteryVolume { get; set; }
         public string CorpusType { get; set; }
     }
