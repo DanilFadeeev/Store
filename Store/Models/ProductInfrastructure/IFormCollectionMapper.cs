a40c0270bdf5fb6834f524dee169710cf4d399ef
Danila <fdanya0@gmail.com>
2020-12-20 23:37:30 +0300

    move files to another folder

---
+++ Store/Models/ProductInfrastructure/IFormCollectionMapper.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store.Models.ProductInfrastructure
+{
+    public interface IFormCollectionMapper
+    {
+        T Map<T>(IFormCollection data) where T:Product, new();
+    }
+}
