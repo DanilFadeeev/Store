bc0b8834e501b7863b4467a25183804be8a73fcb
Danila <fdanya0@gmail.com>
2020-12-20 22:19:11 +0300

    create controller for add products

---
+++ Store/Controllers/AddProductController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using Store.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store.Controllers
+{
+    public class AddProductController:Controller
+    {
+        public ICategoryRepository CategoryRepo { get; }
+
+        public AddProductController(ICategoryRepository categoryRepo)
+        {
+            CategoryRepo = categoryRepo;
+        }
+        [HttpGet]
+        public async Task<IActionResult> AddProduct(string productType = null)
+        {
+            var types = await CategoryRepo.GetNotAbstractChildren("product");
+            ViewData["productType"] = productType;
+            return View(types);
+        }
+
+        [HttpPost]
+        public IActionResult addproduct(IFormCollection data)
+        {
+            if (data["category"].Contains("meatGrinder"))
+                return View();
+            //new ProductRepository().GetProducts<Product>("a");
+            return View();
+        }
+    }
+}
