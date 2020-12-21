dc6b017e800c1ce644f231027b7cced57c5f862d
Danila <fdanya0@gmail.com>
2020-12-21 15:11:56 +0300

    add product saver class

--- Store/Models/ProductInfrastructure/Mapper.cs
+++ Store/Models/ProductInfrastructure/Mapper.cs
@@ -10,12 +10,16 @@ namespace Store.Models.ProductInfrastructure
     {
         public T Map<T>(IFormCollection fieldsData) where T: Product,new()
         {
+            int a;       
             T result = new();
             foreach (var i in fieldsData.Keys)
             {
                 var proprety = result.GetType().GetProperty(i);
                 if (proprety is null)
                     continue;
+                if(proprety.PropertyType == typeof(int))
+                proprety.SetValue(result, int.TryParse(fieldsData[i].First()?.ToString(),out a)? int.Parse(fieldsData[i].First()?.ToString()):0);
+                else
                 proprety.SetValue(result, fieldsData[i].First()?.ToString()??default);
             }
             return result;
--- Store/Models/ProductInfrastructure/ProductSaver.cs
+++ Store/Models/ProductInfrastructure/ProductSaver.cs
@@ -1,29 +1,40 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
 using Store.Models.Products;
+using Store.Utils;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Store.Models.ProductInfrastructure
 {
-    public class ProductSaver : ISaver
+    public class ProductSaver : IProductSaver
     {
         public IFormCollectionMapper Mapper { get; }
         public IInsertSqlComandProvider Sql { get; }
+        public IProductPhotoSaver PhotoSaver { get; }
+        public IConnectionStringProvider ConnectionStringProvider { get; }
 
-        public ProductSaver(IFormCollectionMapper mapper, IInsertSqlComandProvider sql)
+        public ProductSaver(IFormCollectionMapper mapper, IInsertSqlComandProvider sql, IProductPhotoSaver photoSaver, IConnectionStringProvider connectionStringProvider)
         {
             Mapper = mapper;
             Sql = sql;
+            PhotoSaver = photoSaver;
+            ConnectionStringProvider = connectionStringProvider;
         }
 
 
         public void Save(IFormCollection data)
         {
-            object result;
+            Product result=null;
             if (data["Category"] == "meatGrinder")
                 result = Mapper.Map<MeatGrinder>(data);
+            PhotoSaver.Save(data, result);
+
+            SqlConnection conn = new(ConnectionStringProvider.ConnectionString);
+            conn.Execute(Sql.GetCommand(result), result);
         }
     }
 }
