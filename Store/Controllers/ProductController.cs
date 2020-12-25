82436cdf8d404e09a418e13199890e99408c67b1
Danila <fdanya0@gmail.com>
2020-12-25 23:08:03 +0300

    add show product info action

--- Store/Controllers/ProductController.cs
+++ Store/Controllers/ProductController.cs
@@ -85,9 +85,33 @@ namespace Store.Controllers
         }
         public IActionResult AllProducts(string category)
         {
-           var result = ProductRepository.GetProductsWithSalersAsync(category ?? "product").Result;
+            var result = ProductRepository.GetProductsWithSalersAsync(category ?? "product").Result;
             return View(result);
         }
-    
+        public IActionResult ShowProductInfo(int productId)
+        {
+
+            var data = ProductRepository.GetProductById(productId).Result;
+            Product Model = null;
+            if (data is null)
+                return NotFound();
+
+            if (data.Category == "meatGrinder")
+                Model = ProductRepository.GetProductById<MeatGrinder>(data.Id).Result;
+            if (data.Category == "notebook")
+                Model = ProductRepository.GetProductById<Notebook>(data.Id).Result;
+            if (data.Category == "refrigerator")
+                Model = ProductRepository.GetProductById<Refrigerator>(data.Id).Result;
+            if (data.Category == "smartphone")
+                Model = ProductRepository.GetProductById<Smartphone>(data.Id).Result;
+            if (data.Category == "vacuumCleaner")
+                Model = ProductRepository.GetProductById<VacuumCleaner>(data.Id).Result;
+
+            if(Model is not null)
+                return View(data.Category, Model);
+
+
+            return NotFound();
+        }
     }
 }
