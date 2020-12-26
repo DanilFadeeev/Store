3e6564840daae7375846da18f764b8a5130f9cd3
Danila <fdanya0@gmail.com>
2020-12-26 19:34:12 +0300

    add checkout model

---
+++ Store/Models/Checkout.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store.Models
+{
+    public class Checkout
+    {
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string House { get; set; }
+        [DisplayName("apartment number")]
+        public string Apartment { get; set; }
+        public DateTime DeliveryTime { get; set; }
+        public Cart Cart { get; set; }
+    }
+}
