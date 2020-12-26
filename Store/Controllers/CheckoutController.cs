4030896a2e0935e75285df4d4aea5da1cdb791b4
Danila <fdanya0@gmail.com>
2020-12-26 19:45:43 +0300

    add checkout controller

---
+++ Store/Controllers/CheckoutController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        public IActionResult Checkout([FromServices] Cart cartService)
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Checkhout(Checkout order)
+        {
+
+            return Redirect("/");
+        }
+    }
+}
